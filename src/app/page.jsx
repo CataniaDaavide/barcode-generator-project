164991e7a994f2e71865469abf3034ce312bcbbc
Davide Catania <davide.catania@sckt.it>
2025-05-16 12:24:02 +0200

    UPDATA: aggioranta la grafica

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,6 +7,13 @@ export default function BarcodesPage() {
   const [input, setInput] = useState('')
   const [barcodes, setBarcodes] = useState([])
 
+  const handleKeyUp = (e) => {
+    if(e.key === "Enter"){
+      generateBarcodes()
+      return
+    }
+  }
+
   const generateBarcodes = () => {
     const codes = input.split(',').map(code => code.trim()).filter(Boolean)
     const newBarcodes = []
@@ -34,28 +41,29 @@ export default function BarcodesPage() {
       <div className='flex flex-col gap-4'>
         <h1 className="text-5xl text-amber-500 font-bold">Generatore di Barcode</h1>
         <textarea
-          className="w-full p-2 border border-amber-500 rounded-lg"
+          className="w-full md:w-[500px] p-2 border border-amber-500 rounded-lg outline-none"
           rows={4}
           placeholder="Inserisci codici separati da virgola"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyUp={handleKeyUp}
         />
 
       </div>
       <button
-        className="w-42 px-4 py-2 bg-amber-600 rounded hover:bg-amber-700"
+        className="w-42 px-4 py-2 bg-amber-500 rounded hover:bg-amber-600 cursor-pointer active:scale-[0.95]"
         onClick={generateBarcodes}
       >
         Genera
       </button>
 
-      <div className="w-full flex flex-wrap gap-12 items-center justify-center pt-5">
+      <div className="w-full flex flex-wrap gap-12 items-center justify-center md:justify-start pt-5">
         {barcodes.map(({ svg, code }, idx) => (
-          <div key={idx} className="flex flex-col items-center justify-center">
+          <div key={idx} className="w-full md:w-auto flex flex-col items-center justify-center">
             <div className='w-full bg-amber-500 text-center p-1 rounded-t-lg'>
-              <p className="text-xl font-bold">{`Code: ${code}`}</p>
+              <p className="font-semibold">{code}</p>
             </div>
-            <div dangerouslySetInnerHTML={{ __html: svg }} className='bg-white p-3 rounded-b-lg'/>
+            <div dangerouslySetInnerHTML={{ __html: svg }} className='w-full flex justify-center bg-white p-3 rounded-b-lg'/>
           </div>
         ))}
       </div>
